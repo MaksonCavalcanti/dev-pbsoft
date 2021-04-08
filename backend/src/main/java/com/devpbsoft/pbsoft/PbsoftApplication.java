32f0be4a4ffe7918dbffa17182073e296f8b7f63
Makson Emmanuel Araújo de Oliveira Cavalcanti <maksoncavalcanti@gmail.com>
2021-04-08 00:30:48 -0300

    Project created

---
+++ backend/src/main/java/com/devpbsoft/pbsoft/PbsoftApplication.java
@@ -0,0 +1,13 @@
+package com.devpbsoft.pbsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbsoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PbsoftApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devpbsoft/pbsoft/PbsoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devpbsoft.pbsoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbsoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
